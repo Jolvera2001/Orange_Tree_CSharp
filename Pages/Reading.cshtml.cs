913648097b1039380213343559302e2217eae288
Jolvera2001 <jolvera6@stedwards.edu>
2025-07-13 00:04:42 -0500

    made blog reading page with a fallback in case blog doesn't exist or an error happens

---
+++ Pages/Reading.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Ganss.Xss;
+using Markdig;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Orange_Tree.Models;
+using Orange_Tree.services;
+
+namespace Orange_Tree.Pages;
+
+public class Reading(ILogger<IndexModel> logger, BlogService service) : PageModel
+{
+    private readonly HtmlSanitizer  _htmlSanitizer = new HtmlSanitizer();
+    public Blog? Blog { get; set; }
+    
+    public async Task<IActionResult> OnGetAsync(string slug)
+    {
+        var blog = await service.GetBlogBySlugAsync(slug);
+        Blog = blog ?? FallBackBlog();
+        return Page();
+    }
+
+    private string SanitizeBlog(string content)
+    {
+        var parsedContent = Markdown.ToHtml(content);
+        return _htmlSanitizer.Sanitize(parsedContent);
+    }
+
+    private Blog FallBackBlog()
+    {
+        return new Blog()
+        {
+            Title = "Error has Occured",
+            Description = "This either doesn't exist or has been removed",
+            Content = "Sorry for the inconvenience",
+        };
+    }
+}
