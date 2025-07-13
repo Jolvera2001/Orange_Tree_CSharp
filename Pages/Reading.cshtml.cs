2514e1886b34486e00f88a2ea60832f22e4c18ec
Jolvera2001 <jolvera6@stedwards.edu>
2025-07-13 00:32:59 -0500

    added syntax highlighting in code snippets and markdown rendering seems good

--- Pages/Reading.cshtml.cs
+++ Pages/Reading.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Ganss.Xss;
 using Markdig;
+using Markdig.SyntaxHighlighting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Orange_Tree.Models;
@@ -10,6 +11,10 @@ namespace Orange_Tree.Pages;
 public class Reading(ILogger<IndexModel> logger, BlogService service) : PageModel
 {
     private readonly HtmlSanitizer  _htmlSanitizer = new HtmlSanitizer();
+    private MarkdownPipeline _pipeline = new MarkdownPipelineBuilder()
+        .UseAdvancedExtensions()
+        .UseSyntaxHighlighting()
+        .Build();
     public Blog? Blog { get; set; }
     
     public async Task<IActionResult> OnGetAsync(string slug)
@@ -26,7 +31,7 @@ public class Reading(ILogger<IndexModel> logger, BlogService service) : PageMode
 
     private string SanitizeBlog(string content)
     {
-        var parsedContent = Markdown.ToHtml(content);
+        var parsedContent = Markdown.ToHtml(content, _pipeline);
         return _htmlSanitizer.Sanitize(parsedContent);
     }
 }
